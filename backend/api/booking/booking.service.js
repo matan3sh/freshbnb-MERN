d92182d6c5dc5ea83acace803c130600206fbdce
Matan Shaviro <matan3sh@gmail.com>
2020-10-04 14:00:09 +0300

    Structure changes to booking validation

--- backend/api/booking/booking.service.js
+++ backend/api/booking/booking.service.js
@@ -4,30 +4,28 @@ const Booking = require('../../models/booking');
 add = async (bookingData, res) => {
   const booking = new Booking(bookingData);
   booking.user = res.locals.user;
-
   if (!_checkIfBookingDatesAreValid(booking))
     return res.sendApiError({
       title: 'Invalid Booking',
       detail: 'Dates are Invalid!',
     });
-
-  Booking.find({ rental: booking.rental }, (error, rentalBookings) => {
-    if (error) return res.mongoError(error);
+  try {
+    const rentalBookings = await Booking.find({ rental: booking.rental });
     const isValid = _checkIfBookingIsValid(booking, rentalBookings);
     if (isValid) {
-      booking.save((error, savedBooking) => {
-        if (error) return res.mongoError(error);
-        return res.json({
-          startAt: savedBooking.startAt,
-          endAt: savedBooking.endAt,
-        });
+      const savedBooking = await booking.save();
+      return res.json({
+        startAt: savedBooking.startAt,
+        endAt: savedBooking.endAt,
       });
     } else
       return res.sendApiError({
         title: 'Invalid Booking',
         detail: 'Choosen dates already taken!',
       });
-  });
+  } catch (error) {
+    return res.mongoError(error);
+  }
 };
 
 _checkIfBookingDatesAreValid = (booking) => {
@@ -43,10 +41,8 @@ _checkIfBookingIsValid = (pendingBooking, rentalBookings) => {
     isValid = rentalBookings.every((booking) => {
       const pendingStart = moment(pendingBooking.startAt);
       const pendingEnd = moment(pendingBooking.endAt);
-
       const bookingStart = moment(booking.startAt);
       const bookingEnd = moment(booking.endAt);
-
       return (
         (bookingStart < pendingStart && bookingEnd < pendingStart) ||
         (pendingEnd < bookingEnd && pendingEnd < bookingStart)
