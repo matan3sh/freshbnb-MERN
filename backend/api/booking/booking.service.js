97c36e9ccc61eb0059a3ee93e1f7f2bc18f1374a
Matan Shaviro <matan3sh@gmail.com>
2020-10-04 12:59:24 +0300

    Implement booking validation in backend

--- backend/api/booking/booking.service.js
+++ backend/api/booking/booking.service.js
@@ -1,8 +1,16 @@
+const moment = require('moment');
 const Booking = require('../../models/booking');
 
 add = async (bookingData, res) => {
   const booking = new Booking(bookingData);
   booking.user = res.locals.user;
+
+  if (!_checkIfBookingDatesAreValid(booking))
+    return res.sendApiError({
+      title: 'Invalid Booking',
+      detail: 'Dates are Invalid!',
+    });
+
   Booking.find({ rental: booking.rental }, (error, rentalBookings) => {
     if (error) return res.mongoError(error);
     const isValid = _checkIfBookingIsValid(booking, rentalBookings);
@@ -14,12 +22,38 @@ add = async (bookingData, res) => {
           endAt: savedBooking.endAt,
         });
       });
-    } else return res.json({ message: 'Booking is NOT Created!' });
+    } else
+      return res.sendApiError({
+        title: 'Invalid Booking',
+        detail: 'Choosen dates already taken!',
+      });
   });
 };
 
-_checkIfBookingIsValid = (booking, rentalBookings) => {
-  return true;
+_checkIfBookingDatesAreValid = (booking) => {
+  let isValid = true;
+  if (!booking.startAt || !booking.endAt) isValid = false;
+  if (moment(booking.startAt) > moment(booking.endAt)) isValid = false;
+  return isValid;
+};
+
+_checkIfBookingIsValid = (pendingBooking, rentalBookings) => {
+  let isValid = true;
+  if (rentalBookings && rentalBookings.length > 0) {
+    isValid = rentalBookings.every((booking) => {
+      const pendingStart = moment(pendingBooking.startAt);
+      const pendingEnd = moment(pendingBooking.endAt);
+
+      const bookingStart = moment(booking.startAt);
+      const bookingEnd = moment(booking.endAt);
+
+      return (
+        (bookingStart < pendingStart && bookingEnd < pendingStart) ||
+        (pendingEnd < bookingEnd && pendingEnd < bookingStart)
+      );
+    });
+  }
+  return isValid;
 };
 
 module.exports = {
